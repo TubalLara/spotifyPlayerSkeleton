39158b2dc00822a15f0f0fb8bb19f6c0b297ffd9
Tubal Lara <tublargar@gmail.com>
2015-08-07 16:27:17 +0200

    fourth and bonus iteration done

--- main.js
+++ main.js
@@ -5,7 +5,10 @@ function fetchSongs (event) {
     
   function handleSongs (songs) {
     console.log(songs);
-    $('#song_author').text(songs.tracks.items[0].artists[0].name);
+    //$('#song_author').text(songs.tracks.items[0].artists[0].name);
+    var name_image = '<button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal" id="' + songs.tracks.items[0].artists[0].name + '">'
+  	+ songs.tracks.items[0].artists[0].name + '</button>';
+    $('#song_author').append(name_image);
     $('#song_title').text(songs.tracks.items[0].name);
     $('.cover').append("<img src=" + songs.tracks.items[0].album.images[0].url + ">");
     var fileName = songs.tracks.items[0].preview_url;
@@ -22,6 +25,24 @@ function fetchSongs (event) {
   request.fail(handleError);
 }
 
+function fetchArtists (event) {
+  event.preventDefault()
+  var request = $.get('https://api.spotify.com/v1/search?type=artist&query=' + $(".btn-primary").text());
+    
+  function handleArtists (artists) {
+    var artist_searched = artists.artists.items[0];
+    var name_image = '<ul><li><h3>'+ artist_searched.name + '<h3></li><li><img class="img-responsive" src ="' + artist_searched.images[0].url + '"></li><ul>';
+    $('.modal-body').append(name_image);
+    
+  }
+
+  function handleError (err) {
+    console.error('OH NO!!', err);
+  }
+
+  request.done(handleArtists);
+  request.fail(handleError);
+}
 // Define a function to print the player's current time
 function printTime () {
   var current = $('.js-player').prop('currentTime');
@@ -46,3 +67,5 @@ $('.btn-play').click(function(){
 	  	console.log('pausing');
   	}
 });
+
+$('#myModal').on('shown.bs.modal', fetchArtists);
